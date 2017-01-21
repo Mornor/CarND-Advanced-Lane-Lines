f7cd85c077b5ba1ae2bce26759569390587fee52
Mornor <cesliens@gmail.com>
2017-01-21 11:24:25 +0100

    Add stuff

---
+++ src/process.py
@@ -0,0 +1,10 @@
+'''
+Author: Celien Nanson [cesliens@gmail.com]
+This file contains the logic pipeline which will be used to process the images
+received by the camera feed to draw the lines region on the road. 
+'''
+
+import utils
+
+# Calibrate the cameras based on ./camera_cal images
+utils.calibrate_camera()

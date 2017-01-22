abe22999de40ddef2997f1b570fd2e53e24efcdd
Celien Nanson <c.nanson@sofort.com>
2017-01-22 19:10:53 +0100

    Compose different thresholds to detect lines

--- tests/test.py
+++ tests/test.py
@@ -10,7 +10,7 @@ def test_undistort():
 	calibration_images = utils.load_images(PATH_CAMERA_CAL)
 	img_points, obj_points, nx, ny = utils.get_imgpoints_objpoints(calibration_images)
 	undistorted_image = utils.undistort_image(calibration_images[2], obj_points, img_points, nx, ny)
-	utils.plot_diff_images(calibration_images[2], undistorted_image)
+	utils.plot_diff_images(calibration_images[2], undistorted_image, False)
 
 def test_abs_sobel_thresh():
 	original_image = cv2.imread(PATH_TEST_IMAGES + 'test3.jpg')
@@ -26,11 +26,19 @@ def test_mag_thresh():
 
 def test_dir_threshold(): 
 	original_image = cv2.imread(PATH_TEST_IMAGES + 'test3.jpg')
-	thresholded_image = utils.mag_thresh(original_image, sobel_kernel=15, thresh=(0.7, 1.3))
+	thresholded_image = utils.dir_thresh(original_image, sobel_kernel=15, thresh=(0.7, 1.3))
 	original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
 	utils.plot_diff_images(original_image, thresholded_image, True)
 
+def test_get_composed_tresholded_image():
+	original_image = cv2.imread(PATH_TEST_IMAGES + 'test3.jpg')
+	thresholded_image = utils.get_composed_tresholded_image(original_image)
+	original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+	utils.plot_diff_images(original_image, thresholded_image, True)
+
+
 # test_undistort()
-# test_abs_sobel_thresh()
+test_abs_sobel_thresh()
 # test_mag_thresh()
-test_dir_threshold()
+# test_dir_threshold()
+# test_get_composed_tresholded_image()

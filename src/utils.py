8f3144be756e715a7bc9dd0b57ae539f1e6a009a
Celien Nanson <c.nanson@sofort.com>
2017-02-02 19:50:07 +0100

    Remove useless comments

--- src/utils.py
+++ src/utils.py
@@ -28,7 +28,7 @@ def plot_peaks_histogram(image):
 
 	leftx_current = leftx_base
 	rightx_current = rightx_base
-	
+
 	# Set the width of the windows +/- margin
 	margin = 100
 	# Set minimum number of pixels found to recenter window
@@ -81,6 +81,9 @@ def plot_peaks_histogram(image):
 	left_fit = np.polyfit(lefty, leftx, 2)
 	right_fit = np.polyfit(righty, rightx, 2)
 
+	return left_fit, right_fit
+
+	'''
 	# Generate x and y values for plotting
 	ploty = np.linspace(0, image.shape[0]-1, image.shape[0] )
 	left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
@@ -94,52 +97,7 @@ def plot_peaks_histogram(image):
 	plt.xlim(0, 1280)
 	plt.ylim(720, 0)
 	plt.show()
-
-
-
-'''
-Custom method
-def plot_peaks_histogram(image):
-
-	slices = slice_image(image)
-
-	# Contains coordinates of the lines
-	coordinates_left_line_x = []
-	coordinates_left_line_y = []
-	coordinates_right_line_x = []
-	coordinates_right_line_y = []
-
-	for slice in slices:
-		# Get the histogram per slice (sum the pixel value, coulumn wise)
-		histogram = np.sum(slice, axis=0)
-
-		middle_point = np.int(histogram.shape[0]/2)
-
-		# For the x-coordinates, append the max index found in the histogram 
-		coordinates_left_line_x.append(np.argmax(histogram[:middle_point]))
-		coordinates_right_line_x.append(np.argmax(histogram[middle_point:]) + middle_point)
-
-		# For the y coordinates, append the center of the current slice
-		y_center = np.int(slice.shape[0]/2)
-		coordinates_left_line_y.append(y_center)
-		coordinates_right_line_y.append(y_center)
-
-	# Fit a second order polynomial
-	left_fit = np.polyfit(coordinates_left_line_y, coordinates_left_line_x, 2)
-	right_fit = np.polyfit(coordinates_right_line_y, coordinates_right_line_x, 2)
-	
-	# Generate x and y values for plotting
-	fity = np.linspace(0, image.shape[0]-1, image.shape[0] )
-	fit_leftx = left_fit[0]*fity**2 + left_fit[1]*fity + left_fit[2]
-	fit_rightx = right_fit[0]*fity**2 + right_fit[1]*fity + right_fit[2]
-	
-	plt.plot(fit_leftx, fity, color='blue')
-	plt.plot(fit_rightx, fity, color='blue')
-	plt.xlim(0, 1280)
-	plt.ylim(720, 0)
-
-	plt.show()
-'''
+	'''
 
 def slice_image(image, slices=10):
 	'''
